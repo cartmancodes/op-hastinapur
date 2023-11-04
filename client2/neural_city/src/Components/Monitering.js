1687662452e5b31c6cd0e2c5deea83aadb08a678
milangupta95 <milangupta95@gmail.com>
2023-11-04 22:52:07 +0530

    Fixed:gitignore issues

--- client2/neural_city/src/Components/Monitering.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Outlet } from 'react-router'
-function Monitering() {
-  return (
-    <Outlet/>
-  )
-}
-
-export default Monitering
--- client2/neural_city/src/Components/Planning.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Planning() {
-  return (
-    <div>Planning</div>
-  )
-}
-
-export default Planning
